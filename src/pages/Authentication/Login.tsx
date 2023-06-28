7f58d09adcf7718b744f647a1d5efb6d36cd13f2
ChinmayK <kalegaonkar.chinmay1@gmail.com>
2023-06-28 16:35:22 +0530

    login form fields

--- src/pages/Authentication/Login.tsx
+++ src/pages/Authentication/Login.tsx
@@ -1,43 +1,60 @@
-import { Link } from 'react-router-dom';
-
+import { Link } from "react-router-dom";
+import LoginIcon from "@mui/icons-material/Login";
 // material-ui
-import { Box, Button, Card, CardActions, CardContent, Grid, Stack, Typography } from '@mui/material';
+import {
+  Box,
+  Button,
+  Card,
+  CardActionArea,
+  CardActions,
+  CardContent,
+  Grid,
+  Stack,
+  TextField,
+  Typography,
+} from "@mui/material";
+import { useState } from "react";
 
+const style = {
+  card: {
+    height: "40rem",
+    width: "40rem",
+    padding: "1rem",
+  },
+};
 
-const bull = (
-  <Box
-    component="span"
-    sx={{ display: 'inline-block', mx: '2px', transform: 'scale(0.8)' }}
-  >
-    •
-  </Box>
-);
-
-const Login: React.FC<any> = ({sx}) => {
-  return(
-    <Card sx={{ minWidth: 275 }}>
+const Login: React.FC<any> = ({ sx }) => {
+  const handleLogin = (event: any) => {
+    event.preventDefault();
+    console.log(event.target);
+  };
+  return (
+    <Card sx={style.card} variant="outlined">
       <CardContent>
-        <Typography sx={{ fontSize: 14 }} color="text.secondary" gutterBottom>
-          Word of the Day
-        </Typography>
-        <Typography variant="h5" component="div">
-          be{bull}nev{bull}o{bull}lent
-        </Typography>
-        <Typography sx={{ mb: 1.5 }} color="text.secondary">
-          adjective
-        </Typography>
-        <Typography variant="body2">
-          well meaning and kindly.
-          <br />
-          {'"a benevolent smile"'}
-        </Typography>
+        <h1>Login</h1>
+
+        <TextField fullWidth label="username" id="username" margin="normal" />
+        <TextField
+          fullWidth
+          label="password"
+          id="password"
+          margin="normal"
+          type="password"
+        />
       </CardContent>
+
       <CardActions>
-        <Button size="small">Learn More</Button>
+        <Button
+          variant="contained"
+          startIcon={<LoginIcon />}
+          onClick={handleLogin}
+        >
+          Login
+        </Button>
+        <Button size="small">Register</Button>
       </CardActions>
     </Card>
-  )
-
-}
+  );
+};
 
 export default Login;
