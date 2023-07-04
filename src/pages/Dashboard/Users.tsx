0f5592651f496f46f82a26c075fdd3377d8e344c
ChinmayK <kalegaonkar.chinmay1@gmail.com>
2023-07-04 14:09:00 +0530

    + : wip sample table

--- src/pages/Dashboard/Users.tsx
+++ src/pages/Dashboard/Users.tsx
@@ -1,5 +1,94 @@
+import {
+  Button,
+  Card,
+  CardActions,
+  CardContent,
+  Checkbox,
+  Container,
+  Grid,
+  Stack,
+  Typography,
+} from "@mui/material";
+
+import { DataGrid, GridColDef, GridValueGetterParams } from "@mui/x-data-grid";
+
+const columns: GridColDef[] = [
+  { field: "id", headerName: "ID", width: 170 },
+  { field: "firstName", headerName: "First name", width: 130 },
+  { field: "lastName", headerName: "Last name", width: 130 },
+  {
+    field: "age",
+    headerName: "Age",
+    type: "number",
+    width: 90,
+  },
+  {
+    field: "fullName",
+    headerName: "Full name",
+    description: "This column has a value getter and is not sortable.",
+    sortable: false,
+    width: 160,
+    valueGetter: (params: GridValueGetterParams) =>
+      `${params.row.firstName || ""} ${params.row.lastName || ""}`,
+  },
+];
+
+const rows = [
+  { id: 1, lastName: "Snow", firstName: "Jon", age: 35 },
+  { id: 2, lastName: "Lannister", firstName: "Cersei", age: 42 },
+  { id: 3, lastName: "Lannister", firstName: "Jaime", age: 45 },
+  { id: 4, lastName: "Stark", firstName: "Arya", age: 16 },
+  { id: 5, lastName: "Targaryen", firstName: "Daenerys", age: null },
+  { id: 6, lastName: "Melisandre", firstName: null, age: 150 },
+  { id: 7, lastName: "Clifford", firstName: "Ferrara", age: 44 },
+  { id: 8, lastName: "Frances", firstName: "Rossini", age: 36 },
+  { id: 9, lastName: "Roxie", firstName: "Harvey", age: 65 },
+];
+
+const SummaryCard = () => {
+  return (
+    <Card sx={{ minWidth: 275 }}>
+      <CardContent>
+        <Typography sx={{ fontSize: 14 }} color="text.secondary" gutterBottom>
+          Total
+        </Typography>
+        <Typography variant="h5" component="div">
+          14
+        </Typography>
+      </CardContent>
+    </Card>
+  );
+};
+
 const Users: React.FC<any> = ({ sx }) => {
-  return <p>users</p>;
+  return (
+    <>
+      <Typography color="text.tertiary" gutterBottom>
+        users
+      </Typography>
+      <Stack direction="row" spacing={8}>
+        <SummaryCard />
+        <SummaryCard />
+        <SummaryCard />
+        <SummaryCard />
+        <SummaryCard />
+      </Stack>
+      <Container maxWidth={false} sx={{ margin: "2rem 0rem" }}>
+        <DataGrid
+          rows={rows}
+          columns={columns}
+          initialState={{
+            pagination: {
+              paginationModel: { page: 0, pageSize: 5 },
+            },
+          }}
+          pageSizeOptions={[5, 10]}
+          checkboxSelection
+          sx={{ color: "white", width: "100%" }}
+        />
+      </Container>
+    </>
+  );
 };
 
 export default Users;
